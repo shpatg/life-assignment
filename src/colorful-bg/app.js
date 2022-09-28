b250615c1c660b515a9ec13f0b988ae87e1fa91d
Fatlum Maliqi <fatlummaliqi49554@gmail.com>
2022-09-28 16:21:47 +0200

    Add event listener on window object

---
+++ src/colorful-bg/app.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const colors = ['blue', 'red', 'green', 'yellow', 'black', 'brown', 'pink']
+    const body = document.querySelector('body')
+  })
