0a787735cfe0a1454e305277335462bd79361802
FlorentinaDermakuu <114822758+FlorentinaDermakuu@users.noreply.github.com>
2022-10-02 13:04:42 +0200

    Update app.js

--- src/Change-Buttons-Color-OnClick/app.js
+++ src/Change-Buttons-Color-OnClick/app.js
@@ -1,5 +1,10 @@
 let btnSuccess = document.querySelector("#success");
+let btnDanger = document.querySelector("#danger");
 
+btnDanger.addEventListener(
+  "click",
+  () => (btnDanger.style.backgroundColor = "#c9302c")
+);
 
 btnSuccess.addEventListener(
   "click",
