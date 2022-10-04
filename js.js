1bef1c9e824ce0b4808304a999358a8b89689112
Donjetë Tifeku <donjete.tifeku@life.gjirafa.com>
2022-10-04 13:17:42 +0200

    Add index.html,style.css and js.js

---
+++ js.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction1() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
