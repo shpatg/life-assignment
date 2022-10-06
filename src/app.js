948b4770c71d53e0b47191b5d755abb53cd31459
Erdison Berisha <79603797+erdisonberisha@users.noreply.github.com>
2022-10-06 18:14:47 +0200

    Change color of div

--- src/app.js
+++ src/app.js
@@ -9,3 +9,8 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+var div = document.querySelector("fixed-bottom p-4");
+div.setAttribute(onclick => {
+  div.setAttribute('style', "background-color: red;")
+})
